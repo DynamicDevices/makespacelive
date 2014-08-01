07e476131564dc880e635721230e3ab4ed89e9a3
alexandros <alexandros@resin.io>
2014-08-01 18:22:39 +0100

    clean up more

--- hello.py
+++ hello.py
@@ -1,26 +1,3 @@
 #!/usr/bin/python
 
-import RPi.GPIO as GPIO
-from time import sleep
-
-# Needs to be BCM. GPIO.BOARD lets you address GPIO ports by periperal
-# connector pin number, and the LED GPIO isn't on the connector
-GPIO.setmode(GPIO.BCM)
-
-GPIO.cleanup()
-
-# set up GPIO output channel
-GPIO.setup(16, GPIO.OUT)
-
-while(1):
-	# Turn On
-	GPIO.output(16, GPIO.LOW)
-
-	# Wait a bit
-	sleep(1000)
-
-	# Turn Off
-	GPIO.output(16, GPIO.HIGH)
-
-	# Wait a bit
-	sleep(1000)
+print "hello python!"
