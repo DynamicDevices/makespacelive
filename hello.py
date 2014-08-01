47cb362b62ced1d5f7fd7ede78606d89e973573c
alexandros <alexandros@resin.io>
2014-08-01 16:32:04 +0100

    blink the LED

--- hello.py
+++ hello.py
@@ -1,1 +1,24 @@
-print "hello python!"
+#!/usr/bin/python
+
+import RPi.GPIO as GPIO
+from time import sleep
+
+# Needs to be BCM. GPIO.BOARD lets you address GPIO ports by periperal
+# connector pin number, and the LED GPIO isn't on the connector
+GPIO.setmode(GPIO.BCM)
+
+# set up GPIO output channel
+GPIO.setup(16, GPIO.OUT)
+
+while(1)
+	# Turn On
+	GPIO.output(16, GPIO.LOW)
+
+	# Wait a bit
+	sleep(100)
+
+	# Turn Off
+	GPIO.output(16, GPIO.HIGH)
+
+	# Wait a bit
+	sleep(100)
