29887b3883e33e0d4d9840d3ddb6f2d49a643859
Alex J Lennon <ajlennon@dynamicdevices.co.uk>
2018-09-29 18:19:00 +0100

    Fixes
    Signed-off-by: Alex J Lennon <ajlennon@dynamicdevices.co.uk>

--- streamwebcam.sh
+++ streamwebcam.sh
@@ -13,7 +13,7 @@ v4l2-ctl --set-ctrl power_line_frequency=1
 v4l2-ctl --set-ctrl auto_exposure=0
 
 gst-launch-1.0 alsasrc device=hw:1 ! audio/x-raw, format=\(string\)S16LE, endianness=\(int\)1234, signed=\(boolean\)true, width=\(int\)16, \
-                depth=\(int\)16, rate=\(int\)16000, channels=\(int\)1 ! queue ! voaacenc bitrate=128 ! aacparse ! audio/mpeg,mpegversion=4,stream-format=raw ! \
+                depth=\(int\)16, rate=\(int\)44100, channels=\(int\)1 ! queue ! voaacenc bitrate=128 ! aacparse ! audio/mpeg,mpegversion=4,stream-format=raw ! \
                 queue ! mux. \
                 v4l2src ! omxh264enc ! video/x-h264,width=1280,height=720,framerate=25/1 ! \
                 h264parse ! flvmux streamable=true name=mux ! queue ! rtmpsink location=${STREAM_URL}/${STREAM_KEY}' live=1 flashver=FME/3.0%20(compatible;%20FMSc%201.0)'
