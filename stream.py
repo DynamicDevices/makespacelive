f17e456026abd383d7e4070dc6ae8c77ef5a9d4b
Alex J Lennon <ajlennon@dynamicdevices.co.uk>
2018-10-19 15:10:36 +0100

    stream.py: add h264 profile=high caps
    Signed-off-by: Alex J Lennon <ajlennon@dynamicdevices.co.uk>

--- stream.py
+++ stream.py
@@ -114,7 +114,7 @@ if __name__ == "__main__":
     Gst.init(None)
 
     audiostr = "alsasrc device=hw:" + str(AUDIO_DEVICE) + " ! audio/x-raw, format=(string)S16LE, endianness=(int)1234, signed=(boolean)true, width=(int)16, depth=(int)16, rate=(int)" + str(AUDIO_SAMPLING_RATE) + " ! queue ! voaacenc bitrate=" + str(AUDIO_BITRATE) + " ! aacparse ! audio/mpeg,mpegversion=4,stream-format=raw ! queue ! mux. "
-    videostr = VIDEO_SOURCE + " ! " + H264_ENCODER + " video/x-h264,width=" +str(VIDEO_WIDTH) + ",height=" + str(VIDEO_HEIGHT) + ",framerate=" + str(VIDEO_FRAMERATE) + "/1 ! h264parse " + H264_PARSER_PARAMS + " ! "
+    videostr = VIDEO_SOURCE + " ! " + H264_ENCODER + " video/x-h264,profile=high,width=" +str(VIDEO_WIDTH) + ",height=" + str(VIDEO_HEIGHT) + ",framerate=" + str(VIDEO_FRAMERATE) + "/1 ! h264parse " + H264_PARSER_PARAMS + " ! "
     muxstr = "flvmux streamable=true name=mux ! queue ! "
     sinkstr = "rtmpsink location='" + STREAM_URL + "/" + STREAM_KEY + " live=1 flashver=FME/3.0%20(compatible;%20FMSc%201.0)'"
 
