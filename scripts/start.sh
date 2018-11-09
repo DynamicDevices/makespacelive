86a9ecfde0a293777e998e248b2a93181c2351cd
Alex J Lennon <ajlennon@dynamicdevices.co.uk>
2018-11-09 15:46:00 +0000

    start.sh: Persist Jupyter notebooks
    Signed-off-by: Alex J Lennon <ajlennon@dynamicdevices.co.uk>

--- scripts/start.sh
+++ scripts/start.sh
@@ -29,6 +29,7 @@ then
 while [ 1 ]
 do
     printf Running Jupyter notebook
+    cd /data
     jupyter notebook --allow-root --ip=0.0.0.0 --port=80 --NotebookApp.token=''
     printf Sleeping...
     sleep 5
